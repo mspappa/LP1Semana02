a54384a8ac2583a5b84952582f26db04a3ace66e
mspappa <marco.duarte.santos@hotmail.com>
2022-03-02 21:38:28 +0000

    commit 6

---
+++ Cilindro/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace OlaMundo
+///
+{
+    class Program
+    ///
+    {
+        static void Main(string[] args)
+        {
+            float pi = 3.1415926; //
+            int raio; 
+            int area; 
+
+            int S = 2 * pi * raio * (raio + area); //
+            int V = pi * (raio * raio) * area; 
+
+            string str = Console.ReadLine();
+        }
+    }
+}
