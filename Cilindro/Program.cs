99835b372383e96027a0751a17a29d46df6db61f
Marco Santos <a22103641@alunos.ulht.pt>
2022-03-03 14:35:14 +0100

    commit aula 1

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -10,17 +10,15 @@ namespace OlaMundo
         static void Main(string[] args)
         {
             int pi = (Convert.ToInt32(3.1415926f)); //
-            int raio; 
-            int altura; 
 
             int S = 2 * pi * raio * (raio + altura); //
             int V = pi * (raio * raio) * altura; 
 
             Console.WriteLine("insira o raio");
-            string str1 = Console.ReadLine();
+            int raio = Console.ReadLine();
 
             Console.WriteLine("insira a altura");
-            string str2 = Console.ReadLine();
+            int altura = Console.ReadLine();
 
             Console.WriteLine(raio + "\n" + altura + "\n");
         }
