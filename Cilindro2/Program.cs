2ed69cc14ee09d4a3ec6f9dc830e29583d562e8e
mspappa <marco.duarte.santos@hotmail.com>
2022-03-03 14:40:45 +0100

    3

---
+++ Cilindro2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Cilindro2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pi = (Convert.ToInt32(3.1415926f)); //
+
+            Console.WriteLine("insira o raio");
+            int raio = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("insira a altura");
+            int altura = int.Parse(Console.ReadLine());
+
+            int S = 2 * pi * raio * (raio + altura); //
+            int V = pi * (raio * raio) * altura; 
+
+            Console.WriteLine(S + "\n" + V + "\n");
+        }
+    }
+}
