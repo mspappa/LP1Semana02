75ddc445e11cfa3a32dacc361e2d35870b4a673d
mspappa <marco.duarte.santos@hotmail.com>
2022-03-03 15:41:05 +0100

    ex5 completo

--- Cilindro2/Program.cs
+++ Cilindro2/Program.cs
@@ -6,7 +6,7 @@ namespace Cilindro2
     {
         static void Main(string[] args)
         {
-            int pi = (Convert.ToInt32(3.1415926f)); //
+            int pi = (Convert.ToInt32(Math.PI)); //
 
             Console.WriteLine("insira o raio");
             int raio = int.Parse(Console.ReadLine());
@@ -14,8 +14,10 @@ namespace Cilindro2
             Console.WriteLine("insira a altura");
             int altura = int.Parse(Console.ReadLine());
 
+            int raio2 = Convert.ToInt32(Math.Pow(raio, 2));
+
             int S = 2 * pi * raio * (raio + altura); //
-            int V = pi * (raio * raio) * altura; 
+            int V = pi * raio2 * altura; 
 
             //mostra no ecra os valores
             Console.WriteLine($"A Superficie do Cilindro e: {S}\n" + $"O Volume do Cilindro e: {V}");
